7ced69766dbf9b860eba8552bd95159d034ebfd7
Rolf Erik Lekang <me@rolflekang.com>
2015-05-09 20:28:00 +0200

    Add sorting of peers

--- src/api.js
+++ src/api.js
@@ -4,6 +4,8 @@ var request = bluebird.promisify(require('browser-request'));
 module.exports.remotes = function remotes(host) {
   return request(host + '/api/clients')
     .spread(function(res, body) {
-      return JSON.parse(body);
+      var peers = JSON.parse(body);
+      peers.sort();
+      return peers;
     });
 };
