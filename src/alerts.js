689ea89dade9238cb002cef3b22f70dc71aa37b2
Rolf Erik Lekang <me@rolflekang.com>
2015-05-09 19:08:30 +0200

    Add get/set form

---
+++ src/alerts.js
@@ -0,0 +1,25 @@
+var $ = require('jquery');
+var closeBtn='<button class="close" data-dismiss="alert">×</button>';
+
+
+function addMessage(message,type){
+  var $alert = $("<div></div>");
+  $alert.addClass("alert");
+  if (type !== "") $alert.addClass(type);
+  $alert.html(closeBtn + "\n" + message);
+  $('#alerts').append($alert);
+}
+
+module.exports = {
+  error: function addError(message){
+    addMessage(message, "alert-error");
+  },
+
+  success: function addSuccess(message){
+    addMessage(message, "alert-success");
+  },
+
+  info: function addInfo(message){
+    addMessage(message, "alert-info");
+  }
+};
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 var Bluebird = require('bluebird');
 var api = require('./api');
+var alerts = require('./alerts');
 
 Bluebird.longStackTraces();
 
@@ -37,6 +38,51 @@ module.exports = function utils(rpc) {
       });
   }
 
+  function formSubmit(event) {
+    event.preventDefault();
+    var $btn = $(this);
+    var $form = $btn.parent();
+    var $buttons = $form.find('button');
+    var $spinner = $form.find('.fa-spinner');
+    var hash = $form.find('#id_hash').val();
+    var promise;
+
+    $spinner.fadeIn();
+    $buttons.addClass('disabled');
+
+    if ($btn.data('action') == 'get') {
+      promise = get(hash, $form.find('#id_key').val());
+    } else if ($btn.data('action') == 'set') {
+      promise = set(hash, $form.find('#id_key').val(), $form.find('#id_value').val());
+    } else {
+      $spinner.fadeOut();
+      $buttons.removeClass('disabled');
+      return;
+    }
+
+    promise.then(function(results) {
+      $spinner.fadeOut();
+      $buttons.removeClass('disabled');
+    });
+  }
+
+
+  function get(hash, key) {
+    return rpc.invoke(hash, 'get', [key])
+      .then(function(result) {
+        alerts.success('Fetch "' + result.value + '" from node "' + result.peer + '"');
+        return result;
+      });
+  }
+
+  function set(hash, key, value) {
+    return rpc.invoke(hash, 'set', [key, value])
+      .then(function(result) {
+        alerts.success('Set');
+        return result;
+      });
+  }
+
   function render($container) {
     var listOfNodes = [];
     for (var key in nodes) {
@@ -48,10 +94,18 @@ module.exports = function utils(rpc) {
       .off('click')
       .on('click', function() {
         var $el = $(this);
+        var hash = $el.data('hash');
         $el.addClass('fa-spin');
-        updateNode($el.data('hash'));
-        $el.removeClass('fa-spin');
+        updateNode(hash, $container)
+          .then(function() {
+            $el.removeClass('fa-spin');
+            $('.nav [data-hash=' + hash + ']').click();
+          });
       });
+
+    $('form button')
+      .off('click')
+      .on('click', formSubmit);
   }
 
   return {
