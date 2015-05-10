7a32cff45bc3e99838355833dcbefec31315eab3
Rolf Erik Lekang <me@rolflekang.com>
2015-05-10 23:24:09 +0200

    Remove dashboards from peers list

--- src/api.js
+++ src/api.js
@@ -5,6 +5,11 @@ module.exports.remotes = function remotes(host) {
   return request(host + '/api/clients')
     .spread(function(res, body) {
       var peers = JSON.parse(body);
+      for (var i = 0; i < clients.length; i++) {
+        if (peers[i].match(/^dashboard/)) {
+          peers.splice(i, 1);
+        }
+      }
       peers.sort();
       return peers;
     });
